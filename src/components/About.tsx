5d989fe672bef921d1edc6a8a5b729a2670356fc
Netflix-Clone <yashvishrest@gmail.com>
2026-08-03 21:26:25 +0530

    Updated About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,11 +6,8 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          Hey! I'm Yashvi - a motivated and enthusiastic CS undergrad with 
-          a dream of becoming a software developer. I contribute to 
-          open-source projects, grind LeetCode (120+ problems!), and am 
-          always excited to learn something new and build something 
-          meaningful. Let's create something great together!
+          I am a Computer Science undergraduate passionate about building AI-powered full-stack applications that solve real-world problems. I enjoy transforming ideas into practical products, from intelligent web applications to AI-integrated tools, while writing clean, scalable, and maintainable code.Driven by curiosity and continuous learning, I enjoy tackling challenging problems, strengthening my software engineering fundamentals, and exploring how AI can create meaningful user experiences. I'm always looking for opportunities to learn, collaborate, and build products that make an impact.
+
         </p>
       </div>
     </div>
